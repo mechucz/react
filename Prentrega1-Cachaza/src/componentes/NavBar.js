2a56ae0a4e09cbff3a4a307c8a4feb6369c9600f
mechucz <mechucz@gmail.com>
2022-10-25 18:50:48 -0300

    filesA

---
+++ Prentrega1-Cachaza/src/componentes/NavBar.js
@@ -0,0 +1,33 @@
+import {BsFillCartFill} from 'react-icons/bs';
+
+const NavBar=()=>{
+  return(<nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+    <a class="navbar-brand" href="#"><img src= "https://mechucz.github.io/mercaditoCool/assets/images/MercaditoCool-1.png" className="fluid"></img>
+Mercadito Cool</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Como Funciona</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Productos</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">Noticias</a>
+        </li>
+        <li className="nav-item">
+         <BsFillCartFill/>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>)
+}
+export default NavBar;
